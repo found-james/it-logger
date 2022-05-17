466c7a46881f945c3f51b08c389b661a0513a74f
James Suescun <james.steven2318@gmail.com>
2022-05-17 10:09:50 -0400

    set up store

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import AddLogModal from "./components/logs/AddLogModal";
 import EditLogModal from "./components/logs/EditLogModal";
 import AddTechModal from "./components/techs/AddTechModal";
 import TechListModal from "./components/techs/TechListModal";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
 
@@ -17,6 +19,7 @@ function App() {
   });
 
   return (
+    <Provider store={store}>
     <Fragment>
       <SearchBar />
       <div className="container">
@@ -27,8 +30,8 @@ function App() {
         <TechListModal />
         <Logs />
       </div>
-      
     </Fragment>
+    </Provider>
   );
 }
 
